073b1c5104f479b2fe83a1dc094be69cf4cbf195
Jesse OConnor <jesseoconnor000@gmail.com>
2024-09-21 10:03:05 +1200

    Parsing expressions

--- TinyParser.hs
+++ TinyParser.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use <$>" #-}
 import Parsing
 
 -- Data types
@@ -21,4 +23,48 @@ data Cmd = Assign Ide Exp
           | WhileDo Exp Cmd
           | Seq Cmd Cmd
           deriving Show
+
+expr :: Parser Exp
+expr =  do e1 <- term
+           do symbol "+"
+              e2 <- expr
+              return (Plus e1 e2)
+        +++
+        do e1 <- term
+           do symbol "="
+              e2 <- expr
+              return (Equal e1 e2)
+        +++
+        term
+
+term :: Parser Exp
+term = do symbol "not"
+          e <- expr
+          return (Not e)
+       +++
+       factor
+
+factor :: Parser Exp
+factor = do symbol "0"
+            return Zero
+         +++
+         do symbol "1"
+            return One
+         +++
+         do symbol "tt"
+            return TT
+         +++
+         do symbol "ff"
+            return FF
+         +++
+         do symbol "read"
+            return Read
+         +++
+         do i <- identifier
+            return (I i)
+         +++
+         do symbol "("
+            e <- expr
+            symbol ")"
+            return e
 
